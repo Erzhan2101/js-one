2db25b3e62f1c22bee8e0d40b95cd16ed1c2e9c0
Erzhan2101 <erzhan210198@gmail.com>
2021-07-02 17:14:42 +0600

    my first js

---
+++ script.js
@@ -0,0 +1,188 @@
+// // .task-1
+// let num = 20
+// console.log(num)
+
+// // task-2
+// let a = 10
+// let b = 2
+// let c = 5
+// console.log(a + b + c)
+
+// // task-3
+// let a = 100
+// console.log(a * 2)
+
+// // task-4
+// let a = 67
+// console.log(a % 7)
+
+// task-5
+// let a = 10
+// let b = 2
+// console.log(10 - 2)
+
+// task-6
+// let a = 15
+// let b = 2
+// let result = a + b
+// console.log(result)
+
+// task-7
+// let a = 30
+// let b = 15
+// let c = a + b
+// console.log(c % 4)
+
+// task 8
+// let a = 17
+// let b = 10
+// console.log(a - b)
+// let c = 7
+// let d = 5
+// let result = c + d
+// console.log(result)
+
+// task-9
+// let a = 10
+// let b = 99
+// let result = a + b
+// console.log(result)
+
+// task-10
+// let name = "Ascar"
+// let number = 10
+// let result = name + number
+// console.log(result)
+
+
+// task-11
+// let a = 20
+// let b = 10
+// console.log(a  b)
+
+// task 12
+// let a = 20
+// let b = 10
+// b = a - b
+// console.log(b)
+
+// task 13
+// let a = 90
+// a = a + 2
+// console.log(a)
+
+// task 14
+// let a = 50
+// a = a * 5
+// console.log(a)
+
+// task 15
+// let a = 660
+// a = a / 2
+// console.log(a)
+
+// task-16
+// let a = 20
+// let b = 2
+// console.log(a % b)
+
+// task-17
+// let a = 28
+// let b = 6
+// console.log(a % b)
+
+// task-18
+// let a = 28
+// let b = 5
+// console.log(a % b)
+
+// task-19
+// let a = 20
+// let b = 21
+// console.log(a > b)
+
+// task-20
+// let a = 20
+// let b = 21
+// console.log(a < b)
+
+// task-21
+// let name = "Erzhan"
+// console.log(name)
+
+// task-22
+// let age = 23
+// console.log(age)
+
+// task-23
+// let str = "Привет, Мир!"
+// console.log(str)
+
+// task-24
+// let str1 = "Приве, "
+// let str2 = "Мир!"
+// console.log(str1 + str2)
+
+// task-25
+// let name = "Erzhan"
+// let result = "Hello " + name
+// console.log(result)
+
+// task-26
+// let age = 30
+// let result = "I'm " + age + " years old"
+// console.log(result)
+
+// task-27
+// let days = 365
+// let planet = "Земля"
+// let num = "7 млрд"
+// let sun = "Солнце"
+// let result = `В нашем году ${days} дней. Днём у нас светит ${sun}. Население планеты ${planet} составляет примерно ${num} человек`
+// console.log(result)
+
+// task-28
+// let greeting = "Welcome, "
+// let name = "Ivan"
+// let result = greeting + name
+// console.log(result)
+
+// task-29
+// let word = "informatics"
+// console.log(word.length)
+
+// task-30
+// let word = "программирование"
+// console.log(word.lastIndexOf("г"))
+
+// task-31
+// let sentences = "Мы выполняем домашнее задание."
+// console.log(sentences.split(' ')[2].length)
+
+// task-32
+// let a = 50
+// console.log(typeof a)
+
+// task-33
+// let a = 10
+// let b = 35
+// let result = a + b + " "
+// console.log(result)
+// console.log(typeof result)
+
+// task-34
+// let a = 10
+// a = a + " "
+// console.log(typeof a)
+
+// task-35
+// let string = " "
+// console.log(typeof string)
+
+// task-36
+// let number = 88
+// console.log(typeof number)
+
+// task-37
+// let boolean = false
+// console.log(boolean)
